4b650390afd9f6c1ca505146a29bd9637b434f09
Rubén <test@test.com>
2017-11-11 21:17:39 +0100

    Testing

--- index.php
+++ index.php
@@ -210,7 +210,7 @@ switch (ENVIRONMENT)
 			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
 		).DIRECTORY_SEPARATOR;
 	}
-	error_log(system_path);
+	error_log($system_path);
 	// Is the system path correct?
 	if ( ! is_dir($system_path))
 	{
